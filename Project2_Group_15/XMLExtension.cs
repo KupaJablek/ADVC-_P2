c52136c4bc9e29e506a2380ee2ca7fdc2ca2892f
Matt <taylor_matt1@hotmail.com>
2023-03-30 14:53:03 -0400

    03-30: finished XMLExtension class/functionality

--- Project2_Group_15/XMLExtension.cs
+++ Project2_Group_15/XMLExtension.cs
@@ -1,23 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Project2_Group_15 {
-    public class XMLExtension {
-        // contain extension methods
-        public void WriteStartDocument() {
+    public static class XMLExtension {
+
+        public static void WriteStartDocument(this XmlWriter writer)
+        {
+            writer.WriteStartDocument();
         }
-        public void WriteStartRootElement() {
+
+        public static void WriteStartRootElement(this XmlWriter writer, string rootTagName)
+        {
+            writer.WriteStartElement(rootTagName);
         }
-        public void WriteEndRootElement() {
+
+        public static void WriteEndRootElement(this XmlWriter writer)
+        {
+            writer.WriteEndElement();
         }
-        public void WriteStartElement() {
+
+        public static void WriteStartElement(this XmlWriter writer, string elementName)
+        {
+            writer.WriteStartElement(elementName);
         }
-        public void WriteEndElement() {
+
+        public static void WriteEndElement(this XmlWriter writer)
+        {
+            writer.WriteEndElement();
         }
-        public void WriteAttribute() { 
+
+        public static void WriteAttribute(this XmlWriter writer, string attributeName, string attributeValue)
+        {
+            writer.WriteAttributeString(attributeName, attributeValue);
         }
     }
 }
